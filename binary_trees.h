43a8d211133488aabd93cad9286405ef406b3099
doloadama <doloadama@outlook.com>
2022-09-25 16:48:32 +0000

    .

--- binary_trees.h
+++ binary_trees.h
@@ -75,17 +75,7 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
-
-
-
-
-
-
-
-
-
-
-
+bst_t *bst_remove(bst_t *root, int value);
 
 
 
