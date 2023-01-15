9ba96c0cb6e6e67a5ef45548502b3f3820cb8cd4
doloadama <doloadama@outlook.com>
2023-01-15 16:59:27 +0000

    .

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,26 +1,46 @@
 #include "binary_trees.h"
-#include "9-binary_tree_height.c"
 
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree.
- * @tree: Pointer 2 the root node of the tree 2 traverse.
- * Return: int -> height of a binary tree
- * 0 -> if tree is NULL
- */
+* binary_tree_height_b - Measures height of a binary tree for a bal tree
+* @tree: tree to go through
+* Return: the height
+*/
+
+size_t binary_tree_height_b(const binary_tree_t *tree)
+{
+size_t l = 0;
+size_t r = 0;
+
+if (tree == NULL)
+{
+return (0);
+}
+else
+{
+if (tree)
+{
+l = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
+r = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
+}
+return ((l > r) ? l : r);
+}
+}
+
+/**
+* binary_tree_balance - Measures balance factor of a binary tree
+* @tree: tree to go through
+* Return: balanced factor
+*/
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-int left_side = 0, right_side = 0;
+int right = 0, left = 0, total = 0;
 
-if (tree == NULL)
-return (0);
-left_side = binary_tree_height(tree->left);
-right_side = binary_tree_height(tree->right);
-
-if (tree->left != NULL)
-left_side++;
-if (tree->right != NULL)
-right_side++;
-
-return (DIFF(left_side, right_side));
+if (tree)
+{
+left = ((int)binary_tree_height_b(tree->left));
+right = ((int)binary_tree_height_b(tree->right));
+total = left - right;
+}
+return (total);
 }
